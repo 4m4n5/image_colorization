4db9648461848d75c7a3c62a739bc92b407bc6b4
Aman Shrivastava <aman.srivastava999@gmail.com>
2019-04-17 15:15:09 -0400

    Writing data_loader

--- data_loader.py
+++ data_loader.py
@@ -1,65 +1,143 @@
+import os
+import os.path as osp
+import sys
+import numpy as np
+import pickle
+from PIL import Image
+import matplotlib.pyplot as plt
+# plt.switch_backend('agg')
+import time
+
 import torch
 import torchvision
 import torchvision.transforms as transforms
+from torch.utils import data
+import scipy.io as io
+import scipy.misc as misc
+import glob
+import csv
+from skimage import color
 
-import cv2
-import numpy as np 
+##### HAVE TO ADD #####
+# from transforms import ReLabel, ToLabel, ToSP, Scale 
 
-########################################################################
-# The output of torchvision datasets are PILImage images of range [0, 1].
-# We transform them to Tensors.
-# tensor_transform = transforms.Compose([transforms.Resize((32, 32)), transforms.ToTensor()])
-tensor_transform = transforms.ToTensor()
+""
+def pil_loader(path):
+    with open(path, 'rb') as f:
+        with Image.open(f) as img:
+            return img.convert('RGB')
 
-# trainset = torchvision.datasets.ImageFolder(root='data/unsplash_cropped_resized/', 
-#                                             transform=tensor_transform)
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=tensor_transform)
+""
+class Unsplash_Dataset(data.Dataset):
+    def __init__(self, root, shuffle=False, small=False, mode='test', transform=None, 
+                 target_transform=None, types='', show_ab=False, large=False, loader=pil_loader):
 
-# classes = ('plane', 'car', 'bbird', 'cat',
-#            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+        tic = time.time()
+        self.root = root
+        self.loader = loader
+        self.image_transform = transform
+        
+#         if large:
+#             self.size = 480
+#             self.imgpath = glob.glob(root + 'img_480/*.png')
+#         else:
+#             self.size = 224
+#             self.imgpath = glob.glob(root + 'img/*.png')
+        
+        self.size = 128
+        self.imgpath = glob.glob(root + '*/*.jpg')
+        
+        self.types = types
+        self.show_ab = show_ab # show ab channel in classify mode
 
-#########################################################################
-# Transform the images to CieLAB color space by the use of OpenCV library.
-rgb_images = []
-numpy_lab_images = []
-for image, label in trainset:
-    rgb_images.append(image)
+        # read split
+        self.train_file = set()
+        
+        with open(self.root + 'train_split.csv', 'r') as f:
+            reader = csv.reader(f, delimiter='\t')
+            for i, row in enumerate(reader):
+                if i == 0:
+                    continue
+                self.train_file.add(str(row[0]).zfill(4))
 
-for rgb_image in rgb_images:
-    numpy_rgb_image = np.transpose(rgb_image.numpy(), (1, 2, 0))
-    numpy_lab_image = cv2.cvtColor(numpy_rgb_image, cv2.COLOR_RGB2LAB)
-    numpy_lab_images.append(numpy_lab_image)
+        assert self.train_file.__len__() == 1392
 
-######################################################################
-# Transform the numpy lab images to images of range [0, 1] and further
-# convert them to tensors.
-lab_images = []
-for numpy_lab_image in numpy_lab_images:
-    numpy_lab_image[:, :, 0] *= 255 / 100
-    numpy_lab_image[:, :, 1] += 128
-    numpy_lab_image[:, :, 2] += 128
-    numpy_lab_image /= 255
-    torch_lab_image = torch.from_numpy(np.transpose(numpy_lab_image, (2, 0, 1)))
-    lab_images.append(torch_lab_image)
+        self.test_file = set()
+        with open(self.root + 'test_split.csv', 'r') as f:
+            reader = csv.reader(f, delimiter='\t')
+            for i, row in enumerate(reader):
+                if i == 0:
+                    continue
+                self.test_file.add(str(row[0]).zfill(4))
+        assert self.test_file.__len__() == 348
 
-#######################################################################
-# Make a custom CieLAB dataset and a data loader that iterates over the
-# custom dataset with shuffling and a batch size of 128.
-class CieLABDataset(torch.utils.data.Dataset):
-    """CieLab dataset."""    
-    def __len__(self):
-        return len(lab_images)
+        self.path = []
+        if mode == 'train':
+            for item in self.imgpath:
+                if item.split('/')[-1][6:6+4] in self.train_file:
+                    self.path.append(item)
+        elif mode == 'test':
+            for item in self.imgpath:
+                if item.split('/')[-1][6:6+4] in self.test_file:
+                    self.path.append(item)
+
+        self.path = sorted(self.path)
+
+        np.random.seed(0)
+        if shuffle:
+            perm = np.random.permutation(len(self.path))
+            self.path = [self.path[i] for i in perm]
+
+        if types == 'classify':
+            ab_list = np.load('data/pts_in_hull.npy')
+            self.nbrs = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(ab_list)
+
+        print('Load %d images, used %fs' % (self.path.__len__(), time.time()-tic))
 
     def __getitem__(self, index):
-        img = lab_images[index]
-        return img
+        mypath = self.path[index]
+        img = self.loader(mypath) # PIL Image
+        img = np.array(img)
+        if (img.shape[0] != self.size) or (img.shape[1] != self.size):
+            img = misc.imresize(img, (self.size, self.size))
 
-cielab_dataset = CieLABDataset()
-cielab_loader = torch.utils.data.DataLoader(cielab_dataset, batch_size=16,
-                  shuffle=True, num_workers=2)
+        img_lab = color.rgb2lab(np.array(img)) # np array
+        # img_lab = img_lab[13:13+224, 13:13+224, :]
 
+        if self.types == 'classify':
+            X_a = np.ravel(img_lab[:,:,1])
+            X_b = np.ravel(img_lab[:,:,2])
+            img_ab = np.vstack((X_a, X_b)).T
+            _, ind = self.nbrs.kneighbors(img_ab)
+            ab_class = np.reshape(ind, (self.size,self.size))
+            # print(ab_class.shape, ab_class.dtype, np.amax(ab_class), np.amin(ab_class))
+            ab_class = torch.unsqueeze(torch.LongTensor(ab_class), 0)
+
+        img = (img - 127.5) / 127.5 # -1 to 1
+        img = torch.FloatTensor(np.transpose(img, (2,0,1)))
+        img_lab = torch.FloatTensor(np.transpose(img_lab, (2,0,1)))
+
+        img_l = torch.unsqueeze(img_lab[0],0) / 100. # L channel 0-100
+        img_ab = (img_lab[1::] + 0) / 110. # ab channel -110 - 110
+
+        if self.types == 'classify':
+            if self.show_ab:
+                return img_l, ab_class, img_ab
+            return img_l, ab_class
+        elif self.types == 'raw':
+            return img_l, img
+            # if self.show_ab:
+            #     return img_l, img_ab, None
+        else:
+            return img_l, img_ab
+
+    def __len__(self):
+        return len(self.path)
+
+
+""
+glob.glob('data/unsplash_cropped_resized/' + '*/*.jpg')[0].zfill(4)
 
 ""
 
