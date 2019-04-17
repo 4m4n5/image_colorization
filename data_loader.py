29b982b2edeadea7d75c1f0ab5d9d6813e665501
Aman Shrivastava <aman.srivastava999@gmail.com>
2019-04-17 17:34:57 -0400

    Added split script and finished dataloader

--- data_loader.py
+++ data_loader.py
@@ -17,6 +17,7 @@ import scipy.misc as misc
 import glob
 import csv
 from skimage import color
+import skimage
 
 ##### HAVE TO ADD #####
 # from transforms import ReLabel, ToLabel, ToSP, Scale 
@@ -30,8 +31,8 @@ def pil_loader(path):
 
 ""
 class Unsplash_Dataset(data.Dataset):
-    def __init__(self, root, shuffle=False, small=False, mode='test', transform=None, 
-                 target_transform=None, types='', show_ab=False, large=False, loader=pil_loader):
+    def __init__(self, root, shuffle=False, mode='test', size=128, transform=None, 
+                 target_transform=None, types='', show_ab=False, loader=pil_loader):
 
         tic = time.time()
         self.root = root
@@ -45,7 +46,7 @@ class Unsplash_Dataset(data.Dataset):
 #             self.size = 224
 #             self.imgpath = glob.glob(root + 'img/*.png')
         
-        self.size = 128
+        self.size = size
         
         self.types = types
         self.show_ab = show_ab # show ab channel in classify mode
@@ -87,7 +88,7 @@ class Unsplash_Dataset(data.Dataset):
         
         # Resize image if necessary
         if (img.shape[0] != self.size) or (img.shape[1] != self.size):
-            img = misc.imresize(img, (self.size, self.size))
+            img = skimage.transform.resize(img, (self.size, self.size))
 
         # Convert to lab space
         img_lab = color.rgb2lab(np.array(img)) # np array
@@ -129,7 +130,7 @@ class Unsplash_Dataset(data.Dataset):
 
 ""
 if __name__ == '__main__':
-    data_root = '/scratch/as3ek/image_colorization/data/unsplash_cropped_resized/'
+    data_root = '/scratch/as3ek/image_colorization/data/unsplash_cropped/'
     # normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
     #                                  std=[0.229, 0.224, 0.225])
 
@@ -138,7 +139,7 @@ if __name__ == '__main__':
                           ])
 
     und = Unsplash_Dataset(data_root, mode='train',
-                      transform=image_transform, large=True, types='raw')
+                      transform=image_transform)
 
     data_loader = data.DataLoader(und,
                                   batch_size=32,
@@ -146,34 +147,4 @@ if __name__ == '__main__':
                                   num_workers=4)
 
     for i, (data, target) in enumerate(data_loader):
-        print(i, len(und))
-
-""
-glob.glob('data/unsplash_cropped_resized/' + '*/*.jpg')[0].zfill(4)
-
-""
-img = pil_loader('data/unsplash_cropped_resized/people/people_pFstlKzYPNY.jpg')
-
-""
-img = np.array(img)
-
-""
-img_lab = color.rgb2lab(np.array(img))
-
-""
-img_lab = torch.FloatTensor(np.transpose(img_lab, (2,0,1)))
-
-""
-img_l = torch.unsqueeze(img_lab[0], 0) / 100.
-
-""
-torch.max(img_l)
-
-""
-img_ab = (img_lab[1: : ] + 0) / 128.
-
-""
-img_ab
-
-""
-
+        print(i, data.size())
