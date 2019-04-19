89d940267f231f6111d4b0ec03170da5ac1d748c
Aman Shrivastava <aman.srivastava999@gmail.com>
2019-04-19 15:16:33 -0400

    Error remove model, start train.py

--- model.py
+++ model.py
@@ -54,7 +54,7 @@ class up(nn.Module):
         if bilinear:
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
         else:
-            self.up = nn.ConvTranspose2d(in_ch//2, in_ch//2, 2,100 stride=2)
+            self.up = nn.ConvTranspose2d(in_ch//2, in_ch//2, 2, stride=2)
 
         self.conv = double_conv(in_ch, out_ch)
 
