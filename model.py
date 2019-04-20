b24f713a0c066f195ce555898320d074ee008667
Aman Shrivastava <aman.srivastava999@gmail.com>
2019-04-20 14:17:00 -0400

    Updated Unet architecture

--- model.py
+++ model.py
@@ -36,7 +36,7 @@ class down(nn.Module):
     def __init__(self, in_ch, out_ch):
         super(down, self).__init__()
         self.mpconv = nn.Sequential(
-#             nn.MaxPool2d(2),
+            nn.MaxPool2d(2),
             double_conv(in_ch, out_ch)
         )
 
@@ -54,11 +54,12 @@ class up(nn.Module):
         if bilinear:
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
         else:
-            self.up = nn.ConvTranspose2d(in_ch//2, in_ch//2, 3, stride=2, bias=False)
+            self.up = nn.ConvTranspose2d(in_ch, in_ch//2, 2, stride=2, bias=False)
 
         self.conv = double_conv(in_ch, out_ch)
 
     def forward(self, x1, x2):
+        
         x1 = self.up(x1)
         
         # input is CHW
@@ -95,24 +96,26 @@ class UNet(nn.Module):
         self.down1 = down(64, 128)
         self.down2 = down(128, 256)
         self.down3 = down(256, 512)
-        self.down4 = down(512, 512)
-        self.up1 = up(1024, 256)
-        self.up2 = up(512, 128)
-        self.up3 = up(256, 64)
+        self.down4 = down(512, 1024)
+        self.up1 = up(1024, 512)
+        self.up2 = up(512, 256)
+        self.up3 = up(256, 128)
         self.up4 = up(128, 64)
         self.outc = outconv(64, n_classes)
 
     def forward(self, x):
-        x1 = self.inc(x)
-        x2 = self.down1(x1)
-        x3 = self.down2(x2)
-        x4 = self.down3(x3)
-        x5 = self.down4(x4)
-        x = self.up1(x5, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
-        x = self.outc(x)
+#         import pdb; pdb.set_trace()
+        # Sample outputs are for an input image size - 32 and bs - 32
+        x1 = self.inc(x) # x ~ [32, 1, 32, 32]
+        x2 = self.down1(x1) # x1 ~ [32, 64, 32, 32]
+        x3 = self.down2(x2) # x2 ~ [32, 128, 16, 16]
+        x4 = self.down3(x3) # x3 ~ [32, 256, 8, 8]
+        x5 = self.down4(x4) # x4 ~ [32, 512, 4, 4], x5~ [32, 1024, 2, 2]
+        x = self.up1(x5, x4) # [32, 512, 4, 4]
+        x = self.up2(x, x3) # [32, 256, 8, 8]
+        x = self.up3(x, x2) # [32, 128, 16, 16]
+        x = self.up4(x, x1) # [32, 64, 32, 32]
+        x = self.outc(x) # [32, 2, 32, 32]
         return torch.tanh(x)
 
 
